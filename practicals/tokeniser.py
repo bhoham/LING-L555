5cfab5d84045f9755f6c7aad162f341289cc1db8
bhoham <89415258+bhoham@users.noreply.github.com>
2021-09-22 13:25:13 -0500

    Update tokeniser.py

--- practicals/tokeniser.py
+++ practicals/tokeniser.py
@@ -1,15 +1,19 @@
 import sys
 
+#reads in the segmented text
 text = sys.stdin.read()
 
-
+#adds in spaces to punctuation so they will be read as separate words
 for p in ['!','?','.',',','(',')','"',]:
 	text = text.replace(p, ' ' + p + ' ')
 
+#creates a counter variable for sentence id
 sent_id = 0
 
+#makes the text a list so it can be read line by line
 text = text.split('\n')
 
+#prints out each line, and each words, giving out the number for each
 for line in text:
 	sent_id = sent_id + 1
 	print('\n # sent_id = ' + str(sent_id))
@@ -18,4 +22,4 @@ for line in text:
 	token_id=0
 	for token in tokens:
 		token_id = token_id + 1
-		print(str(token_id) + "\t" + token + '_\t_\t_\t_\t_\t_\t_\t_\t')
+		print(str(token_id) + "\t" + token + '\t_\t_\t_\t_\t_\t_\t_\t_\t')
