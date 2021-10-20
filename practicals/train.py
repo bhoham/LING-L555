956bf6ad40e19f86f632828c4e895bd227b081f6
bhoham <89415258+bhoham@users.noreply.github.com>
2021-10-20 12:47:05 -0500

    Add files via upload

--- practicals/train.py
+++ practicals/train.py
@@ -19,25 +19,17 @@ for line in text.split('\n'):
 		wordcounter[word] = 0
 	if POS not in poscounter:
 		poscounter[POS] = 0
-	wordtagmatrix[word] = POS
-	wordcounter[word] = wordcounter[word] + 1
-	poscounter[POS] = poscounter[POS] + 1
-	tokencounter = tokencounter + 1
-wordposcombolist = []
-for word in wordtagmatrix:
-	wordposcombo = word + ' ' + wordtagmatrix[word]
-	wordposcombolist.append(wordposcombo)
-for wordposcombo in wordposcombolist:
-	if wordposcombo not in wordtagmatrixcount:
-		wordtagmatrixcount[wordposcombo] = 0
-	wordtagmatrixcount[wordposcombo] = wordtagmatrixcount[wordposcombo] + 1
-
-
+	if word not in wordtagmatrix:
+		wordtagmatrix[word] = {}
+	if POS not in wordtagmatrix[word]:
+		wordtagmatrix[word][POS] = 0
+	wordtagmatrix[word][POS]+=1
+	wordcounter[word] += 1
+	poscounter[POS] += 1
+	tokencounter+= 1
 print('# P\tcount\ttag\tform')
 for POS in poscounter:
-	print(str(poscounter[POS]/tokencounter) + '\t'+ str(poscounter[POS]) + '\t' + POS + '\t_')
-for word in wordtagmatrixcount:
-	splitword = word.split(' ')[0] 
-	if splitword in wordcounter:
-		wordnumber = wordcounter[splitword]
-		print(str(wordtagmatrixcount[word]/wordnumber)+'\t'+str(wordtagmatrixcount[word])+'\t'+word.split(' ')[1]+'\t'+splitword)
+	print('%.5f'%(poscounter[POS]/tokencounter)+'\t'+ str(poscounter[POS]) + '\t' + POS + '\t_')
+for word in wordtagmatrix:
+	for POS in wordtagmatrix[word]:
+		print('%.5f'%(wordtagmatrix[word][POS]/wordcounter[word])+'\t'+str(wordcounter[word]) + '\t'+((str(wordtagmatrix[word].keys())).strip("dict_keys(['")).strip("'])")+'\t'+word)
